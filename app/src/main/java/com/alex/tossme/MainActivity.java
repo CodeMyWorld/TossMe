207fd03b5fe79c50b4221a8a9967abe3d70a5f75
Alex Lai <alexlai@softlab.cs.tsukuba.ac.jp>
2015-04-22 18:13:50 +0900

    do that

--- app/src/main/java/com/alex/tossme/MainActivity.java
+++ app/src/main/java/com/alex/tossme/MainActivity.java
@@ -1,17 +1,31 @@
 package com.alex.tossme;
 
 import android.app.Activity;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements SensorEventListener{
+
+    private float[] gravityValue = new float[3];
+    private TextView show;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SensorManager sensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
+        Sensor gravitySensor = sensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY);
+        sensorManager.registerListener(this,gravitySensor,SensorManager.SENSOR_DELAY_UI);
+
+         show = (TextView)findViewById(R.id.hello);
     }
 
 
@@ -35,5 +49,20 @@ public class MainActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        switch (sensorEvent.sensor.getType()){
+            case Sensor.TYPE_GRAVITY:
+                gravityValue = sensorEvent.values.clone();
+                show.setText(String.valueOf(gravityValue[0]));
+                break;
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
     }
 }
