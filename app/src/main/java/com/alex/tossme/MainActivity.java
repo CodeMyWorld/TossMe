fc29da3e49e08e308df2a6181522bd8b8b5107c0
Alex Lai <alexlai@softlab.cs.tsukuba.ac.jp>
2015-05-20 13:35:05 +0900

    almost completed except for button view

--- app/src/main/java/com/alex/tossme/MainActivity.java
+++ app/src/main/java/com/alex/tossme/MainActivity.java
@@ -1,9 +1,6 @@
 package com.alex.tossme;
 
 import android.app.Activity;
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
-import android.content.pm.Signature;
 import android.graphics.Typeface;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
@@ -11,29 +8,35 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.net.Uri;
 import android.os.Bundle;
-import android.util.Base64;
+
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 import com.facebook.*;
 import com.facebook.share.model.ShareLinkContent;
 import com.facebook.share.widget.ShareDialog;
+import java.math.BigDecimal;
 
-import java.security.MessageDigest;
+
 
 
 
 public class MainActivity extends Activity implements SensorEventListener{
 
     private float[] gravityValue = new float[3];
-    private TextView show;
-    private CallbackManager callbackManager;
     private ShareDialog shareDialog;
 
+    private TextView hangtime;
+    private TextView height;
+    private TextView speed;
+
+    private TextView hangtimeValue;
+    private TextView heightValue;
+    private TextView speedValue;
+    private final double g = 9.8;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,40 +44,51 @@ public class MainActivity extends Activity implements SensorEventListener{
         FacebookSdk.sdkInitialize(getApplicationContext());
         setContentView(R.layout.activity_main);
 
-
-
-
         SensorManager sensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
         Sensor gravitySensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         sensorManager.registerListener(this,gravitySensor,SensorManager.SENSOR_DELAY_UI);
 
-
-
-
-        callbackManager = CallbackManager.Factory.create();
         shareDialog = new ShareDialog(this);
 
-
-
-        Button bt = (Button)findViewById(R.id.shareBt);
-        bt.setOnClickListener(new View.OnClickListener() {
+        Typeface tf = Typeface.createFromAsset(getAssets(), "fonts/smarc.TTF");
+        Button shareBt = (Button)findViewById(R.id.shareBt);
+        shareBt.setTypeface(tf);
+        shareBt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if (ShareDialog.canShow(ShareLinkContent.class)) {
                     ShareLinkContent linkContent = new ShareLinkContent.Builder()
-                            .setContentTitle("Hello Facebook")
+                            .setContentTitle("Look at my record!!")
                             .setContentDescription(
-                                    "The 'Hello Facebook' sample  showcases simple Facebook integration")
-                            .setContentUrl(Uri.parse("http://developers.facebook.com/android"))
+                                    "I just throw my tablet into "+heightValue.getText().toString()+" height air")
+                            .setContentUrl(Uri.parse("http://softlab.cs.tsukuba.ac.jp"))
                             .build();
                     shareDialog.show(linkContent);
                 }
             }
         });
 
-        show = (TextView)findViewById(R.id.hello);
-        Typeface tf = Typeface.createFromAsset(getAssets(), "fonts/smarc.TTF");
-        show.setTypeface(tf);
+        Button resetBt = (Button)findViewById(R.id.reset);
+        resetBt.setTypeface(tf);
+        resetBt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                reset();
+            }
+        });
+
+        hangtime = (TextView)findViewById(R.id.hangtime);
+        speed = (TextView)findViewById(R.id.speed);
+        height = (TextView)findViewById(R.id.height);
+        hangtimeValue = (TextView)findViewById(R.id.hangtimeValue);
+        speedValue = (TextView)findViewById(R.id.speedValue);
+        heightValue = (TextView)findViewById(R.id.heightValue);
+        hangtime.setTypeface(tf);
+        speed.setTypeface(tf);
+        height.setTypeface(tf);
+        hangtimeValue.setTypeface(tf);
+        speedValue.setTypeface(tf);
+        heightValue.setTypeface(tf);
     }
 
 
@@ -104,7 +118,6 @@ public class MainActivity extends Activity implements SensorEventListener{
     private boolean flag = false;
     private long startTime = 0;
 
-
     @Override
     public void onSensorChanged(SensorEvent sensorEvent) {
 
@@ -122,12 +135,14 @@ public class MainActivity extends Activity implements SensorEventListener{
                 if(flag && lengthVector > 8){
                     long endTime = System.currentTimeMillis();
                     long durationTime = endTime - startTime;
-                    long seconds = durationTime / 1000;
-                    long milliSeconds = durationTime % 1000;
-                    String result = show.getText().toString();
-                    result += "\n"+seconds+"."+milliSeconds;
-                    show.setText(result);
-                    Toast.makeText(this," "+seconds+"."+milliSeconds,Toast.LENGTH_SHORT).show();
+                    double hangtime = (double)durationTime/1000;
+
+                    String speed = getSpeed(hangtime);
+                    String height = getHeight(hangtime);
+                    String result =hangtime+"s";
+                    hangtimeValue.setText(result);
+                    heightValue.setText(height);
+                    speedValue.setText(speed);
                     flag = false;
                 }
                 break;
@@ -137,5 +152,24 @@ public class MainActivity extends Activity implements SensorEventListener{
     @Override
     public void onAccuracyChanged(Sensor sensor, int i) {
 
+    }
+
+    public String getSpeed(double time){
+        time /= 2;
+        BigDecimal speed = new BigDecimal(g * time);
+        String result = speed.setScale(2,BigDecimal.ROUND_HALF_UP).toString()+"m/s";
+        return result;
+    }
+    public String getHeight(double time){
+        time /= 2;
+        BigDecimal height = new BigDecimal(g * time * time);
+        String result = height.setScale(2,BigDecimal.ROUND_HALF_UP).toString()+"m";
+        return result;
+    }
+
+    public void reset(){
+        hangtimeValue.setText(R.string.initValue);
+        heightValue.setText(R.string.initHeightValue);
+        speedValue.setText(R.string.initSpeedValue);
     }
 }
